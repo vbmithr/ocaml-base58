45f5e29fe04ce5d3b248c1e8fb677a0764fd756a
Vincent Bernardoff <vb@luminar.eu.org>
2017-10-29 11:51:28 +0100

    test: switch to alcotest

--- test/test.ml
+++ test/test.ml
@@ -1,7 +1,7 @@
-open Base58
-open OUnit2
+let assert_equal a b = assert (a = b)
 
-let test_btc ctx =
+let test_btc () =
+  let open Base58 in
   let addr = "mjVrE2kfz42sLR5gFcfvG6PwbAjhpmsKnn" in
   let addr_encoded = of_string_exn addr in
   let addr_decoded = to_string addr_encoded in
@@ -17,7 +17,8 @@ let test_btc ctx =
   let addr_versioned_str = Bitcoin.to_string versioned in
   assert_equal addr addr_versioned_str
 
-let test_tezos ctx =
+let test_tezos () =
+  let open Base58 in
   let addr = "tz1e5dbxuQ1VBCTvr7DUdahymepWcmFjZcoF" in
   let addr_bytes = to_bytes_exn (`Base58 addr) in
   let addr' = of_bytes addr_bytes in
@@ -28,10 +29,12 @@ let test_tezos ctx =
   let addr' = Tezos.(of_string_exn addr |> to_string) in
   assert_equal addr addr'
 
-let suite =
-  "base58" >::: [
-    "test_btc" >:: test_btc ;
-    "test_tezos" >:: test_tezos ;
-  ]
+let suite = [
+  "btc", `Quick, test_btc ;
+  "tezos", `Quick, test_tezos ;
+]
 
-let () = run_test_tt_main suite
+let () =
+  Alcotest.run "base58" [
+    "suite", suite ;
+  ]
